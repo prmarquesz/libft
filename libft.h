87bcfa2faf70723b563b57a17071be612cd936f2
Paulo Marques <paulormarques21@gmail.com>
2021-05-25 18:00:30 -0300

    Feature: construction of the ft_isdigit function
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -19,5 +19,6 @@ size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 
 #endif
