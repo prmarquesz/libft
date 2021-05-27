7fd8f026a61bb0eb4a624b103c9e189a88212094
Paulo Marques <paulormarques21@gmail.com>
2021-05-26 22:55:06 -0300

    Feature: construction of the ft_strrchr function
    - also added the prototype in the header file (libft.h)

---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/26 22:11:53 by proberto          #+#    #+#             */
+/*   Updated: 2021/05/26 22:51:06 by proberto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = ft_strlen(s);
+	while (i > 0)
+	{
+		if (s[i] == c)
+			return ((char *) s + i);
+		i--;
+	}
+	if (s[i] == c || c == '\0')
+		return ((char *) s + i);
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -19,6 +19,7 @@ size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
