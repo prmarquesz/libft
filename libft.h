5caab4c95c9cd367d846faf1375a90b64293c298
Paulo Marques <paulormarques21@gmail.com>
2021-05-28 16:50:54 -0300

    Feature: construction of the ft_memset function
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@
 
 # include	<stddef.h>
 
+void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
