5c6a240a8d33dfc1106b0a0fb8dd5353f0a36f2e
Paulo Marques <paulormarques21@gmail.com>
2021-05-29 02:02:45 -0300

    Feature: construction of the ft_memccpy function
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@
 
 # include	<stddef.h>
 
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
