9e831a3e93fb4df9557adece3a968340d5632c9b
Paulo Marques <paulormarques21@gmail.com>
2021-05-30 13:55:51 -0300

    Feature: construction of the ft_calloc function
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include	<stddef.h>
+# include	<stdlib.h>
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -21,6 +22,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
