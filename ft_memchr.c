b1f4d9b7d3e42b7c13d6c2d06cce95ed017ec515
Paulo Marques <paulormarques21@gmail.com>
2021-05-30 12:16:25 -0300

    Feature: construction of the ft_memchr function
    - also added the prototype in the header file (libft.h)

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/30 11:59:52 by proberto          #+#    #+#             */
+/*   Updated: 2021/05/30 12:13:30 by proberto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	i = 0;
+	ptr = (unsigned char *) s;
+	while (*(ptr + i) && i < n)
+	{
+		if (ptr[i] == (unsigned char) c)
+			return (ptr + i);
+		i++;
+	}
+	return (NULL);
+}
