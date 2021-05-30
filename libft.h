66a5f0fc5c4ade39e28bb6e58930f7a8d7aaa41d
Paulo Marques <paulormarques21@gmail.com>
2021-05-30 12:40:50 -0300

    Feature: construction of the ft_memcmp function
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -28,6 +28,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
