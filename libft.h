19bf81d8c4e0563249c6e434acf67e24d7f88ff9
Paulo Marques <paulormarques21@gmail.com>
2021-06-03 06:16:44 -0300

    Feature: construction of the ft_putstr_fd function
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -25,6 +25,7 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
