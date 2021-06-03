9ed1872859a64da281d05ec8d249fa2b6ecb88b4
Paulo Marques <paulormarques21@gmail.com>
2021-06-03 05:41:49 -0300

    Feature: construction of the ft_putchar_fd func.
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@
 
 # include	<stddef.h>
 # include	<stdlib.h>
+# include	<unistd.h>
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -23,6 +24,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+void	ft_putchar_fd(char c, int fd);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
