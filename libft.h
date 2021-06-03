7a6fc1ae19eb5997e2666f5a491fbf524df9c5e9
Paulo Marques <paulormarques21@gmail.com>
2021-06-03 20:38:17 -0300

    Feature: construction of the ft_itoa function
    and sub-functions:
    - num_chars;
    - str_populate;
    - also added the prototype in the header file (libft.h).

--- libft.h
+++ libft.h
@@ -38,6 +38,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
+char	*ft_itoa(int n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
