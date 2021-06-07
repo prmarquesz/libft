10e5e66cb0b7ea778dcb0f3ddc7006e2705d320e
Paulo Marques <paulormarques21@gmail.com>
2021-06-07 12:44:16 -0300

    Fix: added Null and start >= len(s) protections
    - added Null protection (l 20)
    - removed the fault when 'start >= ft_strlen(s)' (l 22)

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 22:03:30 by proberto          #+#    #+#             */
-/*   Updated: 2021/05/30 22:55:25 by proberto         ###   ########.fr       */
+/*   Updated: 2021/06/07 12:43:17 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	size;
 	char	*substr;
 
+	if (!s)
+		return (NULL);
+	if (start >= (unsigned int) ft_strlen(s))
+		return (ft_strdup(""));
 	size = len + 1;
 	substr = (char *) malloc(size * sizeof(char));
 	if (!substr)
