9cf002ff811d00f1054e34e4f68c3048579e6c63
Paulo Marques <paulormarques21@gmail.com>
2021-06-07 11:40:09 -0300

    Fix and Refactor: removed crash finding a \0
    - added the pointer 'end_s'
    - 'while' iterator refactored

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 11:59:52 by proberto          #+#    #+#             */
-/*   Updated: 2021/05/30 12:13:30 by proberto         ###   ########.fr       */
+/*   Updated: 2021/06/06 02:55:29 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*ptr;
-	size_t			i;
+	unsigned char	*s_aux;
+	unsigned char	*end_s;
 
-	i = 0;
-	ptr = (unsigned char *) s;
-	while (*(ptr + i) && i < n)
+	s_aux = (unsigned char *) s;
+	end_s = (unsigned char *) s + n;
+	while (s_aux < end_s)
 	{
-		if (ptr[i] == (unsigned char) c)
-			return (ptr + i);
-		i++;
+		if (*s_aux == (unsigned char) c)
+			return (s_aux);
+		s_aux++;
 	}
 	return (NULL);
 }
