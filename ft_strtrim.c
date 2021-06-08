b65a93f11429260127e363da4182ea12f9f2da00
Paulo Marques <paulormarques21@gmail.com>
2021-06-07 23:23:37 -0300

    Fix/Refactor: memory leak
    Full code refactoring, removed subfunctions:
    - prefixtrim
    - suffixtrim
    - makestrtrim
    Added:
    Getting prefix and suffix with 'ft_strchr' and 'ft_strrchr' functions,
    and copying with the 'ft_strcpy' function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,107 +6,32 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 14:57:29 by proberto          #+#    #+#             */
-/*   Updated: 2021/06/07 14:11:53 by proberto         ###   ########.fr       */
+/*   Updated: 2021/06/07 23:17:23 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	prefixtrim(const char *s1, const char *set, size_t len);
-static size_t	suffixtrim(const char *s1, const char *set, size_t len_s1,
-					size_t len_set);
-static char		*makestrtrim(const char *s1, char *strtrim, size_t start,
-					size_t end);
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len_s1;
-	size_t	len_set;
-	size_t	len_strtrim;
-	size_t	offsets[2];
+	size_t	len;
+	size_t	offset;
+	size_t	end;
 	char	*strtrim;
 
 	if (!s1 || !set)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
-	len_set = ft_strlen(set);
-	offsets[0] = prefixtrim(s1, set, len_set);
-	offsets[1] = 0;
-	len_strtrim = 1;
-	if (offsets[0] != len_s1)
-	{
-		offsets[1] = suffixtrim(s1, set, len_s1 - 1, len_set);
-		len_strtrim = len_s1 - offsets[0] - (len_s1 - offsets[1]) + 1;
-	}
-	strtrim = (char *) malloc(len_strtrim * sizeof(char));
+	end = ft_strlen(s1);
+	offset = 0;
+	len = 0;
+	while (offset < end && ft_strchr(set, s1[offset]))
+		offset++;
+	while (end > offset && ft_strrchr(set, s1[end - 1]))
+		end--;
+	len = (end - offset) + 1;
+	strtrim = (char *) malloc(len * sizeof(char));
 	if (!strtrim)
 		return (NULL);
-	makestrtrim(s1, strtrim, offsets[0], len_strtrim);
-	return (strtrim);
-}
-
-static size_t	prefixtrim(const char *s1, const char *set, size_t len)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (*(s1 + i))
-	{
-		j = 0;
-		while (j < len)
-		{
-			if (s1[i] == set[j])
-				j = len + 1;
-			else
-				j++;
-		}
-		if (j == len)
-			return (i);
-		i++;
-	}
-	return (i);
-}
-
-static size_t	suffixtrim(const char *s1, const char *set, size_t len_s1,
-size_t len_set)
-{
-	size_t	i;
-	size_t	j;
-	int		stop;
-
-	i = len_s1;
-	stop = 1;
-	while (stop > 0)
-	{
-		j = 0;
-		while (j < len_set)
-		{
-			if (s1[i] == set[j])
-				j = len_set + 1;
-			else
-				j++;
-		}
-		if (j == len_set)
-			return (i);
-		i--;
-		if (i == 0)
-			stop = -1;
-	}
-	return (i);
-}
-
-static char	*makestrtrim(const char *s1, char *strtrim, size_t start,
-size_t end)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < end)
-	{
-		strtrim[i] = s1[start + i];
-		i++;
-	}
-	strtrim[i] = '\0';
+	ft_strlcpy(strtrim, &s1[offset], len);
 	return (strtrim);
 }
