eca79a7fd0840ed82b6a6b86e8318e98abe0c612
Paulo Marques <paulormarques21@gmail.com>
2021-06-09 04:36:09 -0300

    Feature: ft_lstmap bonus function construction
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 int		ft_lstsize(t_list *lst);
