2b6947a35e3fe975477a13bb4b95f0eb570be3ee
Paulo Marques <paulormarques21@gmail.com>
2021-06-09 01:32:28 -0300

    Feature: ft_lstlast bonus function construction
    - also added the prototype in the header file (libft.h)

--- libft.h
+++ libft.h
@@ -25,6 +25,7 @@ typedef struct s_list
 }					t_list;
 
 t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
 int		ft_lstsize(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
